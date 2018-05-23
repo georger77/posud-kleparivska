7ac00331cdbd8401c0a1acbfc885fb2ddce952d5
George Rezvanov <rezvanov@ukr.net>
2018-05-23 11:58:27 +0000

    Initial

---
+++ js/form.js
@@ -0,0 +1,27 @@
+$(function() {
+    
+    $('form input[type=submit]').click(function(e) {
+        
+        const form=$('form')[0];
+        if(!form.checkValidity()){
+            return;     
+        }
+       e.preventDefault();
+        $.ajax({
+            url: "https://formspree.io/rezvanovgr@gmail.com",
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+                clientEmail: $('#client-email').val(),
+                 clientPhone: $('#client-phone').val(),
+               order: $('#order').val()
+            },
+            dataType: "json" 
+        }).done(function() {
+            form.reset();
+            $('#message-box').html('<h2>Ваша інформація відправлена, чекайте на відповідь!</h2>');
+        }).fail(function(){
+            $('#message-box').html('Помилка при відправленні, спробуйте ще раз');
+        });
+    });
+});
